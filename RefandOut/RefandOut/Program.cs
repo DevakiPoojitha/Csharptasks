a6edd227d65563328480389be0dc1bb1f21dd0cf
Devaki Poojitha <68280793+DevakiPoojitha@users.noreply.github.com>
2022-12-04 20:29:27 +0530

    Add files via upload

---
+++ RefandOut/RefandOut/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+
+public class Program
+{
+    public static void update(out int a)
+    {
+        a = 10;
+    }
+
+    public static void change(ref int d)
+    {
+        d = 11;
+    }
+
+    public static void Main()
+    {
+        int b;
+        int c = 9;
+        update(out b);
+        change(ref c);
+        Console.WriteLine("Updated value is: {0}", b);
+        Console.WriteLine("Changed value is: {0}", c);
+    }
+}
+
+
